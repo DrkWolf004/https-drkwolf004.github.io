8bf3dea2376362a52251b3dce5e5ad963ee98a52
Wolf <gabriel.guzman2201@alumnos.ubiobio.cl>
2026-03-23 16:12:53 -0300

    Update base path for GitHub Pages

--- ProjectPage/ProjectPage/vite.config.js
+++ ProjectPage/ProjectPage/vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/https-drkwolf004.github.io/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
