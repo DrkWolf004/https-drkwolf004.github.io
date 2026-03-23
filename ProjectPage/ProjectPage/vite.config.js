5927cb4bb1f355d440b77ed0476daad4faa09665
Wolf <gabriel.guzman2201@alumnos.ubiobio.cl>
2026-03-23 16:15:33 -0300

    Fix asset paths for GitHub Pages

--- ProjectPage/ProjectPage/vite.config.js
+++ ProjectPage/ProjectPage/vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/https-drkwolf004.github.io/",
+  base: "/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
