07f32d180664429e0af66b7e074513f7e1674dcd
Gabriel <gabriel.guzman2201@alumnos.ubiobio.cl>
2026-04-11 23:51:20 -0400

    Agregar cambios para ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/https-drkwolf004.github.io/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
